c59ac9169864295e3d4b19e464d79f7d18368909
Glsm-pkts <gulsumpektass@gmail.com>
2024-04-10 17:32:24 +0300

    readme düzenlendi

--- app.js
+++ app.js
@@ -4,23 +4,43 @@ import { UI } from "./scripts/ui.js";
 
 //!githup class alma
 const github = new Github();
+
 //! UI classın örneği
 const ui = new UI();
+github.fetchUserData();
 
 const getInput = (e) => {
-    e.preventDefault();
-    const value = elements.searchInput.value;
-    if(value == "") 
-        return;
-   if (value) {
+  e.preventDefault();
+  const value = elements.searchInput.value;
+  if (value == "") {
+    ui.showAlert("Form alanını doldurunuz.", "alert alert-warning");
+    return;
+  }
+  
+  if(value) {
     github.fetchUserData(value).then((res) => {
-      if(res.message === "Not Found" ){
-      ui.showAlert("Aradığınız kullanıcı bulunamadı.","alert alert-danger" );
-    }
+      //!eğer kullanıcı bulunamadıysa 
+      if(res.message === "Not Found"){
+        ui.showAlert("Aradığınız kullanıcı bulunamadı","alert alert-danger")
+        //!kullanıcı bulunduysa
+      }else{
+        ui.showAlert("Kullanıcı bulundu", "alert alert-success");
+        ui.renderProfile(res.data);
+        console.log(res);
+        ui.renderProjects(res.repos)
+        
+        
+      }
   });
-   
+  
+  return;
+
+  }
 }
- };
+
+
+
+
 //! olay izleyicileri
 
 elements.searchBtn.addEventListener("click", getInput);
--- scripts/api.js
+++ scripts/api.js
@@ -1,24 +1,30 @@
 export class Github {
-    constructor() {
-      this.client_id = "ac6586af7bc0155ab2b6";
-      this.client_secret = "2602029d67d0ef51afe43ce634ed3a8f10e50226";
-      this.per_page = 10;
-      this.sort = "asc";
-    }
-    //* api'den kullanıcı bilgilerini alma
-    async fetchUserData(username) {
-      // parametre olarak gelen kullanıcı adına göre istek attık
-      const profileRes = await fetch(
-        `https://api.github.com/users/${username}?client_id=${this.client_id}&client_secret=${this.client_secret}`
-      );
-      // kullanıcı repolarını almak için istek attık
-      const repoRes = await fetch(
-        `https://api.github.com/users/${username}/repos?cliend_id=${this.client_id}&client_secret=${this.client_secret}&per_page=${this.per_page}&sort=${this.sort}`
-      ); 
-      // apiden aldığımız cevabı json yapısına çevirdik
-    const data = await profileRes.json();
-    const repos = await repoRes.json();
-    // fonksiyonun çağrıldığı yere bilgileri gönderme
-    return { data, repos };
+  constructor(){
+    this.client_id = "15f5e620d23e89321b20";
+    this.client_secret = "9bfabb180a9b655a61087e2f070c943d86f1d118";
+    this.per_page = 10;
+    this.sort = "asc"
   }
-};
+  //*api'den kullanıcı bilgilerini alma
+  async fetchUserData(username) { 
+
+    //*parametre olarak gelen kullanıcı adına göre istek attık 
+    const profileRes = await fetch(
+      `https://api.github.com/users/${username}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+//* kullanıcının repolarını almak için
+const repoRes = await fetch(
+  `https://api.github.com/users/${username}/repos?cliend_id=${this.client_id}&client_secret=${this.client_secret}&per_page=${this.per_page}&sort=${this.sort}`
+);
+
+//*Apiden aldığımız cevabı json yapısına çevirdik
+const data = await profileRes.json();
+const repos = await repoRes.json();
+
+
+
+//fonksiyonun çağrıldığı yere bilgileri gönderme
+return {data, repos}
+  }
+}
+    
